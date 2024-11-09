93163bf608213f4e13b13e54e8b4d0eb1b9309ce
Harry_cmartin <harrysoncampos@gmail.com>
2024-11-09 18:03:21 -0300

    add

---
+++ Grafos1/dificeis/Questions/questa2dificil.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_N 100005
+
+typedef struct Node {
+    int vertex;
+    struct Node* next;
+} Node;
+
+Node* graph[MAX_N];
+int visited[MAX_N];
+
+// Função para adicionar uma aresta ao grafo
+void addEdge(int u, int v) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->vertex = v;
+    newNode->next = graph[u];
+    graph[u] = newNode;
+
+    newNode = (Node*)malloc(sizeof(Node));
+    newNode->vertex = u;
+    newNode->next = graph[v];
+    graph[v] = newNode;
+}
---
+++ Grafos1/dificeis/questao2dificil.md
@@ -0,0 +1,3 @@
+## 1377. Frog Position After T Seconds
+
+https://leetcode.com/problems/frog-position-after-t-seconds/description/
